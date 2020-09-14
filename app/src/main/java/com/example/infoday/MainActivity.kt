3c9f1097341335293e5450891d962a5511b34366
Jc_revenger <kenpetere@gmail.com>
2020-09-14 19:28:22 +0800

    lab 2

--- app/src/main/java/com/example/infoday/MainActivity.kt
+++ app/src/main/java/com/example/infoday/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.infoday
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -18,9 +20,25 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.newsListFragment, R.id.eventsFragment, R.id.infoFragment, R.id.mapsFragment))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        findNavController(R.id.nav_host_fragment).popBackStack()
+        return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //The SwitchPreferenceCompat will automatically store the preference value in a key-value dictionary.
+        if (getSharedPreferences("${packageName}_preferences",0).getBoolean("dark_mode", false)) {
+            //switch to night mode
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            //switch to day mode
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
---
+++ app/src/main/java/com/example/infoday/data/News.kt
@@ -0,0 +1,4 @@
+package com.example.infoday.data
+
+data class News (val image: String, val title: String, val detail: String) {
+}
